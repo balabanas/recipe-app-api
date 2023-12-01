b87feb3781d87117d1cac0e5e4348424d937c559
Anton Balabanov <asbbnv@gmail.com>
2023-12-01 09:46:46 +0200

    add user admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,24 @@
-from django.contrib import admin  # noqa
+"""
+Django admin customization
+"""
 
-# Register your models here.
+from django.contrib import admin
+from . models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ('last_login',)
+
+
+admin.site.register(User, UserAdmin)
+
